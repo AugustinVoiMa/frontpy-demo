c8ad5c875ede76b4cf1fb4ac3751ca7edef5faca
Augustin <augustinvoima@gmail.com>
2023-04-22 12:33:23 +0200

    change demo to text editor with save button and edit_text WIP

--- src/frontpy_demo/src/text_editor/ui/mainWindow.py
+++ src/frontpy_demo/src/text_editor/ui/mainWindow.py
@@ -7,23 +7,11 @@ class MainWindowController(FrameController):
 
     def __init__(self):
         super(MainWindowController, self).__init__()
-        self.counter_value = 0
-        self.counter_view: TextView = None
 
     def on_create(self):
         self.set_content_view(R.layout.editor_frame)
         super(MainWindowController, self).on_create()
 
-        self.width = 500
+        self.width = 1200
+        self.height = 600
         self.title = "Text Editor"
-
-        self.counter_value = 0
-
-        btn_inc: ButtonView = self.find_view_by_id(R.id.btn_inc_counter)
-        btn_inc.on_click_listener = self.increment_counter
-
-        self.counter_view: TextView = self.find_view_by_id(R.id.counter)
-
-    def increment_counter(self):
-        self.counter_value += 1
-        self.counter_view.text = str(self.counter_value)
